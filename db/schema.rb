f96d12dde0155742160705acd32dab1877a515fc
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-10-08 16:43:38 -0700

    Add Passenger model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_08_233919) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_08_234020) do
   create_table "airports", force: :cascade do |t|
     t.string "code", null: false
     t.datetime "created_at", null: false
@@ -37,6 +37,13 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_08_233919) do
     t.datetime "updated_at", null: false
     t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
     t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
