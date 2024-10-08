798c2a8e9085a5b57114c8f1d7409d2a2ebf8947
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-10-07 22:48:40 -0700

    Add Flight Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_07_041518) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_07_043326) do
   create_table "airports", force: :cascade do |t|
     t.string "code", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["code"], name: "index_airports_on_code", unique: true
+  end
+
+  create_table "flights", force: :cascade do |t|
+    t.integer "departure_airport_id", null: false
+    t.integer "arrival_airport_id", null: false
+    t.datetime "start_datetime", null: false
+    t.integer "duration", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
+    t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,4 +40,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_07_041518) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "flights", "airports", column: "arrival_airport_id"
+  add_foreign_key "flights", "airports", column: "departure_airport_id"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,32 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-[ 'SEA', 'LAX', 'NYC', 'HNL', 'MIA', 'SFO', 'ATL', 'ORD' ].each do |airport_code|
+airport_codes = [ 'SEA', 'LAX', 'NYC', 'HNL', 'MIA', 'SFO', 'ATL', 'ORD' ]
+airport_codes.each do |airport_code|
   Airport.find_or_create_by!(code: airport_code)
+end
+
+airports = Airport.all
+
+flights = [
+  { departure_airport: airports.find { |a| a.code == 'SEA' }, arrival_airport: airports.find { |a| a.code == 'LAX' }, start_datetime: DateTime.now + 1.day, duration: 120 },
+  { departure_airport: airports.find { |a| a.code == 'LAX' }, arrival_airport: airports.find { |a| a.code == 'NYC' }, start_datetime: DateTime.now + 2.days, duration: 300 },
+  { departure_airport: airports.find { |a| a.code == 'NYC' }, arrival_airport: airports.find { |a| a.code == 'HNL' }, start_datetime: DateTime.now + 3.days, duration: 600 },
+  { departure_airport: airports.find { |a| a.code == 'MIA' }, arrival_airport: airports.find { |a| a.code == 'ORD' }, start_datetime: DateTime.now + 4.days, duration: 150 },
+  { departure_airport: airports.find { |a| a.code == 'SFO' }, arrival_airport: airports.find { |a| a.code == 'ATL' }, start_datetime: DateTime.now + 5.days, duration: 180 },
+  { departure_airport: airports.find { |a| a.code == 'ORD' }, arrival_airport: airports.find { |a| a.code == 'MIA' }, start_datetime: DateTime.now + 6.days, duration: 200 },
+  { departure_airport: airports.find { |a| a.code == 'HNL' }, arrival_airport: airports.find { |a| a.code == 'SEA' }, start_datetime: DateTime.now + 7.days, duration: 480 },
+  { departure_airport: airports.find { |a| a.code == 'NYC' }, arrival_airport: airports.find { |a| a.code == 'SFO' }, start_datetime: DateTime.now + 8.days, duration: 360 },
+  { departure_airport: airports.find { |a| a.code == 'ATL' }, arrival_airport: airports.find { |a| a.code == 'LAX' }, start_datetime: DateTime.now + 9.days, duration: 300 },
+  { departure_airport: airports.find { |a| a.code == 'LAX' }, arrival_airport: airports.find { |a| a.code == 'ORD' }, start_datetime: DateTime.now + 10.days, duration: 240 },
+  { departure_airport: airports.find { |a| a.code == 'MIA' }, arrival_airport: airports.find { |a| a.code == 'HNL' }, start_datetime: DateTime.now + 11.days, duration: 480 }
+]
+
+flights.each do |data|
+  Flight.find_or_create_by!(
+    departure_airport: data[:departure_airport],
+    arrival_airport: data[:arrival_airport],
+    start_datetime: data[:start_datetime],
+    duration: data[:duration]
+  )
 end
