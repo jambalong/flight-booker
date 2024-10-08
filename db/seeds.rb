d473aa58719ab0bd0a72f501b1b0fe5ea07eb13e
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-10-07 23:29:19 -0700

    Tweak Flight seeds for more data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,27 +13,23 @@ airport_codes.each do |airport_code|
   Airport.find_or_create_by!(code: airport_code)
 end
 
-airports = Airport.all
+airports = Airport.all.to_a
+flight_dates = (0..5).map { |i| DateTime.now + i.days }
 
-flights = [
-  { departure_airport: airports.find { |a| a.code == 'SEA' }, arrival_airport: airports.find { |a| a.code == 'LAX' }, start_datetime: DateTime.now + 1.day, duration: 120 },
-  { departure_airport: airports.find { |a| a.code == 'LAX' }, arrival_airport: airports.find { |a| a.code == 'NYC' }, start_datetime: DateTime.now + 2.days, duration: 300 },
-  { departure_airport: airports.find { |a| a.code == 'NYC' }, arrival_airport: airports.find { |a| a.code == 'HNL' }, start_datetime: DateTime.now + 3.days, duration: 600 },
-  { departure_airport: airports.find { |a| a.code == 'MIA' }, arrival_airport: airports.find { |a| a.code == 'ORD' }, start_datetime: DateTime.now + 4.days, duration: 150 },
-  { departure_airport: airports.find { |a| a.code == 'SFO' }, arrival_airport: airports.find { |a| a.code == 'ATL' }, start_datetime: DateTime.now + 5.days, duration: 180 },
-  { departure_airport: airports.find { |a| a.code == 'ORD' }, arrival_airport: airports.find { |a| a.code == 'MIA' }, start_datetime: DateTime.now + 6.days, duration: 200 },
-  { departure_airport: airports.find { |a| a.code == 'HNL' }, arrival_airport: airports.find { |a| a.code == 'SEA' }, start_datetime: DateTime.now + 7.days, duration: 480 },
-  { departure_airport: airports.find { |a| a.code == 'NYC' }, arrival_airport: airports.find { |a| a.code == 'SFO' }, start_datetime: DateTime.now + 8.days, duration: 360 },
-  { departure_airport: airports.find { |a| a.code == 'ATL' }, arrival_airport: airports.find { |a| a.code == 'LAX' }, start_datetime: DateTime.now + 9.days, duration: 300 },
-  { departure_airport: airports.find { |a| a.code == 'LAX' }, arrival_airport: airports.find { |a| a.code == 'ORD' }, start_datetime: DateTime.now + 10.days, duration: 240 },
-  { departure_airport: airports.find { |a| a.code == 'MIA' }, arrival_airport: airports.find { |a| a.code == 'HNL' }, start_datetime: DateTime.now + 11.days, duration: 480 }
-]
+flight_dates.each do |flight_date|
+  airports.permutation(2).each do |departure_airport, arrival_airport|
+    2.times do |i|
+      random_hour = rand(0..23)
+      random_minute = rand(0..59)
 
-flights.each do |data|
-  Flight.find_or_create_by!(
-    departure_airport: data[:departure_airport],
-    arrival_airport: data[:arrival_airport],
-    start_datetime: data[:start_datetime],
-    duration: data[:duration]
-  )
+      start_datetime = flight_date.change(hour: random_hour, min: random_minute)
+
+      Flight.find_or_create_by!(
+        departure_airport: departure_airport,
+        arrival_airport: arrival_airport,
+        start_datetime: start_datetime,
+        duration: rand(60..600)
+      )
+    end
+  end
 end
