7a952ce82496e0748f0255bf68481f7a16cfa976
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-10-08 21:16:27 -0700

    Add Bookings#new and #create actions

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,25 @@
+class BookingsController < ApplicationController
+  def new
+    @flight = Flight.find(params[:selected_flight])
+    @booking = Booking.new(passenger_count: params[:passenger_count])
+    @booking.passenger_count.times { @booking.passengers.build }
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to @booking, notice: "Booking was created!"
+    else
+      @flight = Flight.find(params[:booking][:flight_id])
+      @booking.passenger_count.times { @booking.passengers.build }
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, :user_id, :passenger_count, passengers_attributes: [ :name, :email ])
+  end
+end
